b380246feb4f816a9678be78c26e9e0ad976d860
Manon <deschamps.manon.91@gmail.com>
2017-04-02 00:00:04 +0200

    initialize on precompile false

--- config/application.rb
+++ config/application.rb
@@ -6,5 +6,6 @@ Bundler.require(*Rails.groups)
 module Watermelon
   class Application < Rails::Application
     config.middleware.use I18n::JS::Middleware
+    config.assets.initialize_on_precompile = false
   end
 end
