d14e5adfc305248efec46929664580a19328a2c2
Manon <deschamps.manon.91@gmail.com>
2017-04-12 10:44:59 +0200

    add devise css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
   protect_from_forgery with: :exception
   before_action :set_locale
   before_filter :set_current_user, if: :current_user
@@ -24,6 +26,14 @@ class ApplicationController < ActionController::Base
   end
 
   private
+    def layout_by_resource
+      if devise_controller?
+        'devise'
+      else
+        'application'
+      end
+    end
+
     def extract_locale_from_accept_language_header
       request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
     end
